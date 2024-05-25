5371b4c40c6d5d1ac1dc60625002547f758f81b1
YD <7942yongdae@gmail.com>
2024-05-25 16:10:18 +0900

    Modify: 폴더 구조 변경 및 라이브러리 업데이트
    - React, Spring 버전 최신화
    - Root > App, Server 구조 분리

--- server/src/main/java/com/example/server/DemoApplication.java
+++ server/src/main/java/com/example/server/DemoApplication.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.example.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(DemoApplication.class, args);
+  }
 
 }
---
+++ server/src/main/java/com/example/server/HelloWorldController.java
@@ -0,0 +1,15 @@
+package com.example.server;
+
+import java.util.Arrays;
+import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+  @GetMapping("hello")
+  public List<String> hello() {
+    return Arrays.asList("안녕하세요", "Hello");
+  }
+}
